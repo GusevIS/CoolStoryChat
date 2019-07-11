0c8ff3a790f96706e45bc32d2b61e6af7e924308
GusevIS <vanya.ivan.gusev@mail.ru>
2019-07-11 12:53:12 +0300

    add prototype of Server

---
+++ Server/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.concurrent.Semaphore;
+
+public class Main {
+    public static final int MAX_CLIENTS_COUNT = 20;
+    public static final int PORT = 1000;
+    public static Semaphore semaphore;
+
+    public static LinkedList<ServerThread> serverThreadList;
+
+    public static void main(String ... args) throws IOException, InterruptedException {
+        semaphore = new Semaphore(MAX_CLIENTS_COUNT);
+        serverThreadList = new LinkedList<>();
+
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        while (true) {
+            semaphore.acquire();
+            Socket clientSocket = serverSocket.accept();
+            serverThreadList.add(new ServerThread(clientSocket));
+        }
+    }
+}
---
+++ Server/src/ServerThread.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.net.Socket;
+
+public class ServerThread extends Thread {
+    private Socket clientSocket;
+    private static BufferedWriter out;
+
+    public ServerThread(Socket clientSocket){
+        this.clientSocket = clientSocket;
+        start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            serve(clientSocket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            Main.semaphore.release();
+        }
+    }
+
+    private static void serve(Socket clientSocket) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+        while (true) {
+            String msg = in.readLine();
+            if(msg.equals("Log out"))
+                break;
+
+            for(ServerThread thread: Main.serverThreadList)
+                thread.sendMsg(msg);
+        }
+    }
+
+    private void sendMsg(String msg) throws IOException {
+        out.write(msg + "\n");
+        out.flush();
+    }
+}

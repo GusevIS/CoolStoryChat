ad87db5104b1d7c60b9b416d70ffe5768076cc63
GusevIS <vanya.ivan.gusev@mail.ru>
2019-07-18 15:33:25 +0300

    change Server codestyle

--- desktopMessenger/Server/src/main/java/Server.java
+++ desktopMessenger/Server/src/main/java/Server.java
@@ -4,17 +4,21 @@ import java.net.Socket;
 import java.util.LinkedList;
 
 public class Server {
-    private static final int PORT = 4990;
-    private static LinkedList<ServerThread> serverList = new LinkedList<>();
+    private final int PORT = 4990;
+    private LinkedList<ServerThread> serverList;
 
-    public static void main(String[] args) throws IOException {
+    public Server(){
+        serverList = new LinkedList<>();
+    }
+
+    public void startServer() throws IOException {
         ServerSocket server = new ServerSocket(PORT);
         try {
             while (true) {
                 Socket socket = server.accept();
                 try {
                     System.out.println("client connected");
-                    serverList.add(new ServerThread(socket));
+                    serverList.add(new ServerThread(this, socket));
                 } catch (IOException e) {
                     socket.close();
                 }
@@ -24,11 +28,17 @@ public class Server {
         }
     }
 
-    public static LinkedList<ServerThread> getServerList(){
+    public synchronized void sendToEveryone(String message){
+        for (ServerThread thread: serverList) {
+            thread.sendMessage(message);
+        }
+    }
+
+    public LinkedList<ServerThread> getServerList(){
         return serverList;
     }
 
-    public static void removeServerThread(ServerThread thread){
+    public synchronized void removeServerThread(ServerThread thread){
         serverList.remove(thread);
     }
 }
--- desktopMessenger/Server/src/main/java/ServerThread.java
+++ desktopMessenger/Server/src/main/java/ServerThread.java
@@ -6,13 +6,15 @@ import com.google.gson.*;
 
 class ServerThread extends Thread {
     private DatabaseHandler dbHandler;
-    private final Gson gson = new GsonBuilder().create();
+    private Server server;
+    private final Gson GSON = new GsonBuilder().create();
     private boolean clientIsConnectedToChat;
     private boolean clientIsActive;
     private BufferedReader in;
     private BufferedWriter out;
 
-    public ServerThread(Socket socket) throws IOException {
+    public ServerThread(Server server, Socket socket) throws IOException {
+        this.server = server;
         dbHandler = new DatabaseHandler();
         clientIsActive = true;
         clientIsConnectedToChat = false;
@@ -30,17 +32,17 @@ class ServerThread extends Thread {
             while(clientIsActive) {
                 if (!clientIsConnectedToChat) {
                     msg = in.readLine();
-                    authMessage = gson.fromJson(msg, AuthorisationMessage.class);
+                    authMessage = GSON.fromJson(msg, AuthorisationMessage.class);
                     switch (authMessage.getFlag()){
                         case SIGN_IN:
                             if(getUser(authMessage.getUsername(), authMessage.getPassword())) {
-                                String authMsg = gson.toJson(new AuthorisationMessage(authMessage.getUsername(), "", MessageFlag.SUCCESSFUL_SIGN_IN));
+                                String authMsg = GSON.toJson(new AuthorisationMessage(authMessage.getUsername(), "", MessageFlag.SUCCESSFUL_SIGN_IN));
                                 out.write(authMsg + "\n");
                                 out.flush();
                                 clientIsConnectedToChat = true;
                                 System.out.println("Client enter to the chat");
                             }else {
-                                String authMsg = gson.toJson(new AuthorisationMessage("", "", MessageFlag.FAILED_SIGN_IN));
+                                String authMsg = GSON.toJson(new AuthorisationMessage("", "", MessageFlag.FAILED_SIGN_IN));
                                 out.write(authMsg + "\n");
                                 out.flush();
                             }
@@ -48,13 +50,13 @@ class ServerThread extends Thread {
 
                         case SIGN_UP:
                             if(userExists(authMessage.getUsername())) {
-                                String authMsg = gson.toJson(new AuthorisationMessage("", "", MessageFlag.FAILED_SIGN_UP));
+                                String authMsg = GSON.toJson(new AuthorisationMessage("", "", MessageFlag.FAILED_SIGN_UP));
                                 out.write(authMsg + "\n");
                                 out.flush();
                             }
                             else {
                                 dbHandler.signUpUser(authMessage.getUsername(), authMessage.getPassword());
-                                String authMsg = gson.toJson(new AuthorisationMessage("", "", MessageFlag.SUCCESSFUL_SIGN_UP));
+                                String authMsg = GSON.toJson(new AuthorisationMessage("", "", MessageFlag.SUCCESSFUL_SIGN_UP));
                                 out.write(authMsg + "\n");
                                 out.flush();
                             }
@@ -62,18 +64,16 @@ class ServerThread extends Thread {
                     }
                 } else {
                     msg = in.readLine();
-                    clientMessage = gson.fromJson(msg, ClientMessage.class);
+                    clientMessage = GSON.fromJson(msg, ClientMessage.class);
                     switch (clientMessage.getFlag()){
                         case DEFAULT_MESSAGE:
                             System.out.println(clientMessage.getUsername() + ">> " + clientMessage.getMessage());
-                            for(ServerThread thread : Server.getServerList()){
-                                thread.sendMessage(clientMessage);
-                            }
+                            server.sendToEveryone(msg);
                             break;
 
                         case LOG_OUT_FROM_SERVER:
                             clientIsActive = false;
-                            Server.removeServerThread(this);
+                            server.removeServerThread(this);
 
                             out.write(msg + "\n");
                             out.flush();
@@ -91,10 +91,9 @@ class ServerThread extends Thread {
         }
     }
 
-    private void sendMessage(ClientMessage message) {
+    public void sendMessage(String message) {
         try {
-            String msg = gson.toJson(message);
-            out.write(msg + "\n");
+            out.write(message + "\n");
             out.flush();
         } catch (IOException ignored) {}
     }
@@ -103,9 +102,9 @@ class ServerThread extends Thread {
         ResultSet result = dbHandler.getUser(loginName, loginPass);
         if(result == null) {
             clientIsActive = false;
-            Server.removeServerThread(this);
+            server.removeServerThread(this);
             try {
-                String authMsg = gson.toJson(new ClientMessage("", "", MessageFlag.LOG_OUT_FROM_SERVER));
+                String authMsg = GSON.toJson(new ClientMessage("", "", MessageFlag.LOG_OUT_FROM_SERVER));
                 out.write(authMsg + "\n");
                 out.flush();
                 return false;
@@ -129,9 +128,9 @@ class ServerThread extends Thread {
         if(result == null) {
             if(result == null) {
                 clientIsActive = false;
-                Server.removeServerThread(this);
+                server.removeServerThread(this);
                 try {
-                    String authMsg = gson.toJson(new ClientMessage("", "", MessageFlag.LOG_OUT_FROM_SERVER));
+                    String authMsg = GSON.toJson(new ClientMessage("", "", MessageFlag.LOG_OUT_FROM_SERVER));
                     out.write(authMsg + "\n");
                     out.flush();
                     return false;
